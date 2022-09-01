cc64f35831f73f3cf442304d03db09528baa34ad
jm84 <jignamolina@gmail.com>
2022-09-01 14:16:08 -0400

    creacion de componentes y css

---
+++ src/app/deposit-coins/deposit-coins.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DepositCoinsComponent } from './deposit-coins.component';
+
+describe('DepositCoinsComponent', () => {
+  let component: DepositCoinsComponent;
+  let fixture: ComponentFixture<DepositCoinsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DepositCoinsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DepositCoinsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
