41fd0dcfcc561fb7d416d1b6c9aae299ab359180
jm84 <jignamolina@gmail.com>
2022-09-01 15:37:59 -0400

    add config routes to deposit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BalanceComponent } from './balance/balance.component';
+import { DepositComponent } from './deposit/deposit.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'balance', component: BalanceComponent },
+  { path: 'deposit', component: DepositComponent },
+  { path: 'withdraw', component: WithdrawComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
